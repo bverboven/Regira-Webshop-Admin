be7cece30767681d0e82e4dad9ed4c8ee4bd5142
bverboven <bramverboven@hotmail.com>
2026-03-22 19:38:34 +0100

    Stop tracking regira_modules symlink

--- src/regira_modules/vue/ioc/index.ts
+++
@@ -1,8 +0,0 @@
-export { default, ServiceProvider, get, type IServiceProvider } from "./ServiceProvider"
-export { plugin } from "./plugin"
-
-declare module "@vue/runtime-core" {
-    interface ComponentCustomProperties {
-        $services: import("./ServiceProvider").IServiceProvider
-    }
-}
--- src/regira_modules/vue/online/index.ts
+++
@@ -1,8 +0,0 @@
-export { useOnlineChecker } from "./online-checker"
-export { default as plugin, default } from "./plugin"
-
-declare module "@vue/runtime-core" {
-    interface ComponentCustomProperties {
-        $isOnline: import("vue").Ref<boolean>
-    }
-}
