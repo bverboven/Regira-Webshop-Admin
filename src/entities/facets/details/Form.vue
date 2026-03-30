c32871fdf5ac463462826097812776fcb40232b6
Bram Verboven <bram@regira.com>
2026-03-30 17:41:57 +0200

    layout

--- src/entities/facets/details/Form.vue
+++ src/entities/facets/details/Form.vue
@@ -2,12 +2,10 @@
     <form @submit.prevent="handleSubmit" :modelValue="item">
         <div class="row form-buttons">
             <div class="col col-md-auto order-1">
-                <FormButtonsRow :item="item" :readonly="readonly" :feedback="feedback" :show-delete="item?.id > 0"
-                    @cancel="handleCancel" @remove="handleRemove" @restore="handleRestore" />
+                <FormButtonsRow :item="item" :readonly="readonly" :feedback="feedback" :show-delete="item?.id > 0" @cancel="handleCancel" @remove="handleRemove" @restore="handleRestore" />
             </div>
             <div class="col-auto order-2 order-md-3">
-                <RouterLink v-if="isPopup" :to="{ name: `${Entity.name}Details`, params: { id: item.$id } }"
-                    class="btn btn-default py-1" target="_blank" :title="$t('popOut')">
+                <RouterLink v-if="isPopup" :to="{ name: `${Entity.name}Details`, params: { id: item.$id } }" class="btn btn-default py-1" target="_blank" :title="$t('popOut')">
                     <Icon name="popOut" />
                 </RouterLink>
                 <RouterLink v-else-if="overviewUrl" :to="overviewUrl" class="btn btn-info py-1">
@@ -23,7 +21,7 @@
             <div class="col">
                 <FormSection :title="$t(config.detailsTitle || '')" :readonly="readonly">
                     <div class="row">
-                        <div class="col-md mb-2">
+                        <div class="col mb-2">
                             <div class="input-group">
                                 <div class="input-group-text">
                                     <Icon name="code" />
@@ -32,7 +30,9 @@
                             </div>
                             <FormLabel :label="$t('code')" />
                         </div>
-                        <div class="col-md mb-2">
+                    </div>
+                    <div class="row">
+                        <div class="col mb-2">
                             <div class="input-group">
                                 <div class="input-group-text">
                                     <Icon name="title" />
--- src/entities/unit-types/details/Form.vue
+++ src/entities/unit-types/details/Form.vue
@@ -2,12 +2,10 @@
     <form @submit.prevent="handleSubmit" :modelValue="item">
         <div class="row form-buttons">
             <div class="col col-md-auto order-1">
-                <FormButtonsRow :item="item" :readonly="readonly" :feedback="feedback" :show-delete="item?.id > 0"
-                    @cancel="handleCancel" @remove="handleRemove" @restore="handleRestore" />
+                <FormButtonsRow :item="item" :readonly="readonly" :feedback="feedback" :show-delete="item?.id > 0" @cancel="handleCancel" @remove="handleRemove" @restore="handleRestore" />
             </div>
             <div class="col-auto order-2 order-md-3">
-                <RouterLink v-if="isPopup" :to="{ name: `${Entity.name}Details`, params: { id: item.$id } }"
-                    class="btn btn-default py-1" target="_blank" :title="$t('popOut')">
+                <RouterLink v-if="isPopup" :to="{ name: `${Entity.name}Details`, params: { id: item.$id } }" class="btn btn-default py-1" target="_blank" :title="$t('popOut')">
                     <Icon name="popOut" />
                 </RouterLink>
                 <RouterLink v-else-if="overviewUrl" :to="overviewUrl" class="btn btn-info py-1">
@@ -23,28 +21,25 @@
             <div class="col">
                 <FormSection :title="$t(config.detailsTitle || '')" :readonly="readonly">
                     <div class="row">
-                        <div class="col-md mb-2">
+                        <div class="col mb-2">
                             <div class="input-group">
                                 <div class="input-group-text">
                                     <Icon name="code" />
                                 </div>
-                                <input v-model="item.code" maxlength="8" :readonly="readonly" class="form-control" />
+                                <input v-model="item.code" maxlength="16" :readonly="readonly" class="form-control" />
                             </div>
                             <FormLabel :label="$t('code')" />
-                        </div>
-                        <div class="col-md mb-2">
-                            <div class="input-group">
-                                <div class="input-group-text">
-                                    <Icon name="title" />
-                                </div>
-                                <input v-model="item.title" maxlength="128" :readonly="readonly" class="form-control" />
-                            </div>
-                            <FormLabel :label="$t('name')" />
                         </div>
                     </div>
                     <div class="row">
                         <div class="col mb-2">
-                            <DescriptionInput v-model="item.description" :label="$t('notes')" :readonly="readonly" />
+                            <div class="input-group">
+                                <div class="input-group-text">
+                                    <Icon name="title" />
+                                </div>
+                                <input v-model="item.title" maxlength="64" :readonly="readonly" class="form-control" />
+                            </div>
+                            <FormLabel :label="$t('name')" />
                         </div>
                     </div>
                 </FormSection>
