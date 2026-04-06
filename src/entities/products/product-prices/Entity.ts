5a65d1a49d75778c1f9df38699fb5fa81f4e90ce
bverboven <bramverboven@hotmail.com>
2026-04-06 23:50:05 +0200

    Checkpoint from VS Code for cloud agent session

---
+++ src/entities/products/product-prices/Entity.ts
@@ -0,0 +1,28 @@
+import { EntityBase } from "@/regira_modules/vue/entities";
+import type { Product } from "../data/Entity";
+
+export class ProductPricePeriod extends EntityBase {
+  id: number = 0;
+  objectId: number; // productId
+  price: number;
+
+  startDate: Date;
+
+  product?: Product;
+  _deleted: boolean = false;
+
+  override get $id(): string | number {
+    return this.id || "new";
+  }
+  override get $title(): string | undefined {
+    return this.product?.$title;
+  }
+
+  static create(values?: object): ProductPricePeriod {
+    return Object.assign(new ProductPricePeriod(), values || {});
+  }
+}
+
+export const Entity = ProductPricePeriod;
+
+export default ProductPricePeriod;
